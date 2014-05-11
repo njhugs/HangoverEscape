b4255f661c282790d69c2c291a43e84424136e87
nathodius <njh2986@vt.edu>
2014-05-10 22:58:11 -0400

    update shebang and fix indentation

--- world.py
+++ world.py
@@ -11,7 +11,7 @@ class Room:
     """A single room"""
 
     def __init__(self, data, name):
-	self.name = name #added a name attribute
+        self.name = name #added a name attribute
         self.description = data['desc']
         self.items = data['objects'] # list of item string names
         self.item_list = [] # list of room objects
@@ -20,21 +20,21 @@ class Room:
             #self.item_list[cntr] = Item(self.items[cntr])#Added by Luke
 
     def __str__(self):
-	list_of_strings = [ str(item) for item in self.items ]
+        list_of_strings = [ str(item) for item in self.items ]
         return "room name: " + self.name + "\ndescription: " + self.description + "\nobjects: " +  ", ".join(list_of_strings) # made it so also returns room name b4 desc and items
 
     def get_name(self): #added a method to return room name
-	return self.name
+        return self.name
     
 class World:
     """A world contains one or more rooms"""
 
     def __init__(self, data, current_name):
         self.rooms = {}
-	counter = 0
+        counter = 0
         for key,value in data.items():
             self.rooms[key] = Room(data[key], data.keys()[counter]) # give each room a name by getting dict key
-	    counter = counter + 1 
+            counter = counter + 1 
 
         self._current = self.rooms[current_name]
 
@@ -52,10 +52,10 @@ class World:
     def go(self, data, direction): # put a try, except in for accessing direction
         try:
                 newRoom = data[self._current.get_name()][direction]
-	except KeyError:
-        	print("You can't go: " + direction) 
-	else:
-		self._current = self.rooms[newRoom] #updates the current room object in the world   
+        except KeyError:
+                print("You can't go: " + direction) 
+        else:
+                self._current = self.rooms[newRoom] #updates the current room object in the world   
 
  #   def NoPath(direction):
 #	print("You cannot travel" + direction)
@@ -64,5 +64,5 @@ class NoPath(KeyError):
      def __init__(self, direction):
         self.direction = direction
      def __str__(self):
-	return ("There is no path leading " + self.direction)
+        return ("There is no path leading " + self.direction)
    #def commandInput(self, data)
